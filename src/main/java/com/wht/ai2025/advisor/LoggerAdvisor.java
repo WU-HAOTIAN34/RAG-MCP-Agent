11f7653855c62d96759ee677e08d28c73c5a8100
Haotian Wu <101766788+WU-HAOTIAN34@users.noreply.github.com>
2025-08-11 13:42:30 +0800

    RAG

--- src/main/java/com/wht/ai2025/advisor/LoggerAdvisor.java
+++ src/main/java/com/wht/ai2025/advisor/LoggerAdvisor.java
@@ -1,6 +1,53 @@
 package com.wht.ai2025.advisor;
 
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClientMessageAggregator;
+import org.springframework.ai.chat.client.ChatClientRequest;
+import org.springframework.ai.chat.client.ChatClientResponse;
+import org.springframework.ai.chat.client.advisor.api.CallAdvisor;
+import org.springframework.ai.chat.client.advisor.api.CallAdvisorChain;
+import org.springframework.ai.chat.client.advisor.api.StreamAdvisor;
+import org.springframework.ai.chat.client.advisor.api.StreamAdvisorChain;
+import reactor.core.publisher.Flux;
 
-public class LoggerAdvisor {
+
+@Slf4j
+public class LoggerAdvisor implements CallAdvisor, StreamAdvisor {
+
+
+    @Override
+    public ChatClientResponse adviseCall(ChatClientRequest chatClientRequest, CallAdvisorChain callAdvisorChain) {
+        chatClientRequest = this.logRequest(chatClientRequest);
+        ChatClientResponse chatClientResponse = callAdvisorChain.nextCall(chatClientRequest);
+        this.logResponse(chatClientResponse);
+        return chatClientResponse;
+    }
+
+    @Override
+    public Flux<ChatClientResponse> adviseStream(ChatClientRequest chatClientRequest, StreamAdvisorChain streamAdvisorChain) {
+        this.logRequest(chatClientRequest);
+        Flux<ChatClientResponse> chatClientResponses = streamAdvisorChain.nextStream(chatClientRequest);
+        return (new ChatClientMessageAggregator()).aggregateChatClientResponse(chatClientResponses, this::logResponse);
+    }
+
+
+    private ChatClientRequest logRequest(ChatClientRequest request) {
+        log.info("request: {}", request.prompt());
+        return request;
+    }
+
+    private void logResponse(ChatClientResponse chatClientResponse) {
+        log.info("response: {}", chatClientResponse.chatResponse().getResult().getOutput().getText());
+    }
+
+    @Override
+    public String getName() {
+        return this.getClass().getSimpleName();
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
 }
