ed59c45d6ffd6e9b2a8eb6a6748027c6194dcf4f
Haotian Wu <101766788+WU-HAOTIAN34@users.noreply.github.com>
2025-08-11 13:53:22 +0800

    RAG

--- src/main/java/com/wht/ai2025/app/TestChatClient.java
+++ src/main/java/com/wht/ai2025/app/TestChatClient.java
@@ -1,19 +1,15 @@
 package com.wht.ai2025.app;
 
 
-import com.wht.ai2025.advisor.LogAdvisor;
 import com.wht.ai2025.advisor.Re2Advisor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
-import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.stereotype.Component;
-import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
-import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 
 
 @Component
@@ -29,7 +25,7 @@ public class TestChatClient {
         ChatClient.Builder builder = ChatClient.builder(dashScopeChatModel)
                 .defaultSystem("你是一个厨艺高超的厨师，需要给用户提供一些菜品制作方法和所需食材")
                 .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build(),
-                        new LogAdvisor(),
+
                         new Re2Advisor());
         this.chatClient = builder.build();
     }
